b4ea5948d58e0337cec85fa2c49af85a3c8eae93
Vojta Oupicky <voj.oup@gmail.com>
2018-12-28 19:41:23 +0100

    Fix headings in gatsby post & add link to HP on 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,7 @@
-import React from 'react'
-
-import Layout from '../components/Layout'
-import SEO from '../components/seo'
+import React from 'react';
+import { Link } from 'gatsby';
+import Layout from '../components/Layout';
+import SEO from '../components/seo';
 
 class NotFoundPage extends React.Component {
   render() {
@@ -10,6 +10,7 @@ class NotFoundPage extends React.Component {
         <SEO title="404: Not Found" />
         <h1>No article found</h1>
         <p>Sorry, I haven&#39;t written this article yet... 😭</p>
+        <Link to={'/'}>Back to safety🚀</Link>
       </Layout>
     )
   }
