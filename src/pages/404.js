b809dd5fee4fce31184c15ab3872b32be5bf30ef
Vojta Oupicky <voj.oup@gmail.com>
2018-12-28 15:19:44 +0100

    Edit 404 paage

--- src/pages/404.js
+++ src/pages/404.js
@@ -8,8 +8,8 @@ class NotFoundPage extends React.Component {
     return (
       <Layout location={this.props.location}>
         <SEO title="404: Not Found" />
-        <h1>Not Found</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <h1>No article found</h1>
+        <p>Sorry, I haven&#39;t written this article yet... 😭</p>
       </Layout>
     )
   }
