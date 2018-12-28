dea8bd5bfa0b261d07c8887240509541915c565d
Vojta Oupicky <voj.oup@gmail.com>
2018-12-28 21:57:14 +0100

    Add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,7 +56,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-131451680-2`,
       },
     },
     `gatsby-plugin-feed`,
