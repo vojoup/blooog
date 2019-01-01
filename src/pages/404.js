c352b26cbc97ca8c008dc797c60dfb8c2ceb1220
Vojta Oupicky <voj.oup@gmail.com>
2019-01-01 21:01:22 +0100

    Add loading effect to 404 image

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,12 +7,13 @@ import SEO from '../components/seo';
 class NotFoundPage extends React.Component {
   render() {
     const { data } = this.props;
+    console.log(data.pic404.childImageSharp.fluid);
     return (
       <Layout location={this.props.location}>
         <SEO title="404: Not Found" />
         <h1>No article found</h1>
         <p>Sorry, I haven&#39;t written this article yet...</p>
-        <Img sizes={data.file.pic404.sizes} alt="Dog and person searching" />
+        <Img fluid={data.pic404.childImageSharp.fluid} title="404 image" alt="Dog and person searching" />
         <Link to={'/'}>Back to safety🚀</Link>
         <footer>Picture by <a href="https://icons8.com">https://icons8.com</a></footer>
       </Layout>
@@ -25,10 +26,10 @@ export default NotFoundPage
 
 export const query = graphql`
 query HeaderImageQuery {
-  file(relativePath: {eq: "404.png"}) {
-    pic404: childImageSharp {
-      sizes(maxWidth: 1240) {
-        ...GatsbyImageSharpSizes
+  pic404: file(relativePath: {eq: "404.png"}) {
+    childImageSharp {
+      fluid(maxWidth: 1240) {
+        ...GatsbyImageSharpFluid_tracedSVG
       }
     }
   }
